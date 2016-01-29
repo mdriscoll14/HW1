6e924ff8e85b7b93845559e35ec57bcb0ef8a6dc
Molly Driscoll <mollydriscoll@mollys-macbook-pro-4.local>
2016-01-28 16:50:26 -0800

    About to upload to GitHub

--- BullysEye/ViewController.swift
+++ BullysEye/ViewController.swift
@@ -9,9 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    var targetValue:Int = 0
+    var score = 0
+    var round = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+    
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft"){
+            let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        
+        if let trackRightImage = UIImage(named: "SliderTrackRight"){
+            let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +46,58 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func sliderMoved(slider: UISlider){
+        
+        currentValue = lroundf(slider.value)    }
+    
+    
+    func startNewRound(){
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50;
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    @IBAction func restart(){
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = "\(round)"
+    }
+    
+    @IBAction func showAlert() {
+        let difference = abs(currentValue - targetValue)
+        let points = 100 - difference
+        score += points
+        let message = "You scored \(points)"
+        if points > 85{
+        
+        let alert = UIAlertController(title: "Great Job",
+            message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+     presentViewController(alert, animated: true, completion: nil)
+        startNewRound()
+        }else{
+        let alert = UIAlertController(title: "Close, but not close enough",
+            message: message, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+        score = 0
+        round = 0
+        startNewRound()
+        }
+        
+    }
+    
+
+    
 
 
 }
